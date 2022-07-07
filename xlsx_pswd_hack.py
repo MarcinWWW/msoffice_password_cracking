e36ce2113b1ad48e83708896827a62e063b254b5
Marcin <angielskiegotlumacz@gmail.com>
2022-07-07 12:41:58 +0200

    update Jul 2022
    add parameter

--- xlsx_pswd_hack.py
+++ xlsx_pswd_hack.py
@@ -1,4 +1,11 @@
 #!/usr/bin/env python3
+#if a job was stopped ctrl + c you can continue with a set of checked passwords
+#by running the program with the file of set of passwords as parameter
+#e.g. python xlsx_pswd_hack.py parameter.txt
+
+import sys
+#import xlrd
+#import win32com.client
 
 import pandas as pd
 import io
@@ -20,40 +27,63 @@ def getListOfVariations(ls, c_num_start, c_num_stop):
 	
 	return l
 	
-#####################
-# setup configuration
-charset_start = 0
+def readInProgress(file):
+	ls_ex = []
+	with open(file, 'r') as my_file:
+		for line in my_file.readlines():
+			ls_ex.append(line.rstrip())
+	return ls_ex
+	
+###################	
+#SETUP
+charset_start = 26
 charset_stop = 35
+
+#custom list_string
+#list_string = "930"
+list_string = getListString(charset_start, charset_stop)
+print(list_string)
 
 pass_char_number_start = 1
 pass_char_number_stop = 4
 
 #FILE
-location = '\\input\\abc.xlsx'
+location = 'C:\\Mar\\dokumenty\\Python\\xlsx_hack\\input\\pin_9930.xlsx'
 
-# optional, manual string
-#list_string = "930"
-######################
+####################
 
-list_string = getListString(charset_start, charset_stop)
-print(list_string)
 
 list_of_variations = getListOfVariations(list_string, pass_char_number_start, pass_char_number_stop)
 #print(list_of_variations)
 
-ls_len = len(list_of_variations)
-print("size = " + str(ls_len))
+list_of_exclusions = []
+list_excluded = []
+
+if len(sys.argv) > 1:
+	list_of_exclusions = readInProgress(sys.argv[1])
+	list_excluded = [x for x in list_of_variations if x not in list_of_exclusions]
+else:
+	list_excluded = list_of_variations
+
+ls_excluded_len = len(list_excluded)
+print("TODO variations = " + str(ls_excluded_len))
+#print(list_of_exclusions)
+
 
 file_name = location[location.rfind("\\")+1:location.rfind(".")]
 file_location = location[:location.rfind("input")]
 file_out = open(file_location + "output\\" + file_name + ".txt", "w")
+file_out.writelines([str(l) + '\n' for l in list_of_exclusions])
 
 decrypted_workbook = io.BytesIO()
+
+cstart = charset_start
+cdiff = charset_stop - charset_start + 1
 
 with open(location, 'rb') as file:
 	office_file = msoffcrypto.OfficeFile(file)
 
-	for pswd in list_of_variations:
+	for pswd in list_excluded:
 	
 		office_file.load_key(password=pswd)
 		
@@ -68,9 +98,8 @@ with open(location, 'rb') as file:
 			break
 			
 		except msoffcrypto.exceptions.InvalidKeyError:
-			print(str(ls_len) + ' ' + pswd)
+			print("pswd = " + pswd)
 			file_out.write(pswd + "\n")
-			ls_len -= 1
 		
 file.close()
 file_out.close()
